c4988a44ee0b14df935b066024643387af84cd4d
RODRIGO STEVE <127690844+zRoys@users.noreply.github.com>
2023-11-11 04:17:44 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web server 
+// Load the express module
+var express = require('express');
+// Create our express app
+var app = express();
+// Require body-parser (to receive post data from clients)
+var bodyParser = require('body-parser');
+// Integrate body-parser with our App
+app.use(bodyParser.urlencoded({ extended: true }));
+// Require path
+var path = require('path');
+
+// Setting our Static Folder Directory
+app.use(express.static(path.join(__dirname, './static')));
+// Setting our Views Folder Directory
+app.set('views', path.join(__dirname, './views'));
+// Setting our View Engine set to EJS
+app.set('view engine', 'ejs');
